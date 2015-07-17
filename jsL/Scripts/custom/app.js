2d5366b3da2d0f6269f56c4df55d35d26ccf727e
Louie Gor <kamchuenlouisng@gmail.com>
2015-07-17 16:33:37 -0400

    first commit

---
+++ jsL/Scripts/custom/app.js
@@ -0,0 +1,13 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('MyCtrl', ['$scope', MyCtrl]);
+
+function MyCtrl($scope) {
+    $scope.visible = true;
+
+    $scope.toggle = function () {
+        $scope.visible = !$scope.visible;
+    };
+    
+    $scope.name = "Sashihara Rino";
+}
